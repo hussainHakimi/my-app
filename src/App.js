226eecf5390697b57fdd21b8682f63b48536ff4c
hussainHakimi <hussainbayan614@gmail.com>
2022-07-03 09:58:34 +0430

    changes added.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import {useState} from 'react'
+import AddProduct from './components/AddProduct/AddProduct'
 // import Product from './components/Product/Product'
 import ProductList from './components/ProductList/ProductList'
 
 const App = ()  => {
 
-  const [products, setProducts] = useState(      // two parameters of this function is 1-currentState and 2-function that update state.
-    [
-      {id : '1', title : "Title 1"},
-      {id :'2', title : "Title 2"},
-      {id :'3', title : "Title 3"}
-    ])
+  const [products, setProducts] = useState([])
+    const addProduct = (title) => {
+      const id = Math.floor(Math.random() * 10000);
+      const newProduct = {id , ...title}
 
+      setProducts([...products, newProduct]);
+    }
     const deleteProduct = (id) => {
       setProducts(products.filter((item) => item.id !== id))
     }
   return(
     <div className="container" >
+      <AddProduct onAdd={addProduct}/>
       <ProductList products={products} onDelete={deleteProduct}/>
     </div>
   )
